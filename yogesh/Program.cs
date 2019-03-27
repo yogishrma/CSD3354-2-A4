013a7a0dfb6b10cc85c6cc1523070279ed4987af
visualstudio <visualstudio@122-PC38>
2019-03-27 12:07:08 -0400

    gvrvfr

--- yogesh/Program.cs
+++ yogesh/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace yogesh
+namespace ConsoleApp1
 {
+    class Player
+    {
+        public bool IsAlive
+        {
+            get { return health > 0; }
+        }
+        int health = 100;
+        public void Hit()
+        {
+            Random r = new Random();
+            health -= r.Next(5, 50);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Player player = new Player();
+            for (int i = 0; i < 20; i++)
+            {
+                player.Hit();
+                Console.WriteLine("Is player alive: " + player.IsAlive);
+
+            }
         }
     }
 }
